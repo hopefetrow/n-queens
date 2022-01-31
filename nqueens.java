f82c49a2a8a05ade1666637f1cb0ac04d7cc6b5d
Hope <43889347+hopefetrow@users.noreply.github.com>
2022-01-31 13:09:33 -0500

    Cleaned up code

--- nqueens.java
+++ nqueens.java
@@ -1,14 +1,12 @@
-import java.io.IOException;
 import java.util.Random;
 import java.util.Scanner;
 
 /**
- * -------------------------------------------------------------------------
- * DESCRIPTION: 
- * -------------------------------------------------------------------------
- * Problem:
- * Given an n x n chessboard, how many different ways can you place n queens, 
- * such that no two queens are in the same row, column, and diagonal.
+ * ---------------------------------------------------------------------------
+ * DESCRIPTION:
+ * ---------------------------------------------------------------------------
+ * Problem: Given an n x n chessboard, how many different ways can you place n
+ * queens such that no two queens are in the same row, column, or diagonal.
  * 
  * This program produces all solutions to the n-Queens problem using DFS with
  * backtracking, where once a path is determined to lead to a non-solution, or
@@ -18,41 +16,46 @@ import java.util.Scanner;
  * Before solving, the estimated number of nodes that will be checked before
  * finding all possible solutions is obtained, which is used to estimate the
  * efficiency of backtracking for the n-queens problem.
- * -------------------------------------------------------------------------
- * INPUT: 
- * -------------------------------------------------------------------------
- * The number of queens to place or 0 to quit. 
+ * ---------------------------------------------------------------------------
+ * INPUT:
+ * ---------------------------------------------------------------------------
+ * The number of queens to place or 0 to quit.
  * 
- * 	| This corresponds to number of rows and columns in the chessboard. 
- *	| Example: 4 queens --> 4x4 chessboard
- * -------------------------------------------------------------------------
- * OUTPUT: 
- * -------------------------------------------------------------------------
- * Solutions, total number of solutions, estimate for number of nodes
- * that will be checked, actual number of nodes checked while searching
- *	
- *  
- *	| Each solution consists of an array of integers where each value 
- *	| represents the column placement for the queen in that row number
- *	| (which corresponds to the index). Example: the solution [2 4 1 3]
- *	| for 4-queens problem represents -->
- * 	|  	Queen 1 at index 0 is in row 1, column 2
- * 	|  	Queen 2 at index 1 is in row 2, column 4
- * 	|	Queen 3 at index 2 is in row 3, column 1
- * 	|	Queen 4 at index 3 is in row 4, column 3
+ * This corresponds to number of rows and columns in the chessboard. (i.e. 4
+ * queens results in a 4x4 chessboard)
+ * ---------------------------------------------------------------------------
+ * OUTPUT:
+ * ---------------------------------------------------------------------------
+ * Solutions, total number of solutions, estimate for number of nodes that will
+ * be checked, actual number of nodes checked while searching
+ * 
+ * 
+ * Each solution consists of an array of integers where each value represents
+ * the column placement for the queen in that row number (which corresponds to
+ * the index).
+ * 
+ * The solution [2 4 1 3] for 4-queens problem represents:
+ * 
+ * - Queen 1 at index 0 is in row 1 column 2
+ * 
+ * - Queen 2 at index 1 is in row 2 column 4
+ * 
+ * - Queen 3 at index 2 is in row 3 column 1
+ * 
+ * - Queen 4 at index 3 is in row 4, column 3
  *
  * 
  * @author Hope Fetrow
- * @date June 2021
+ * @since June 2021
  * 
  */
 public class Main {
 	static int col[] = null;
 	// for counts
-	static int total_checked;
-	static int total_solutions;
+	static int checkedCount;
+	static int solutionCount;
 	// for estimate
-	static int[] promising_children;
+	static int[] est;
 	static int n;
 
 	public static void main(String[] args) {
@@ -63,38 +66,35 @@ public class Main {
 		n = input.nextInt();
 		System.out.println();
 
-		// while user input is an int greater than 0
+		// while user input is a number greater than 0
 		while (n > 0) {
-			// print number of queens entered by user
+			// print number of queens that was entered by user
 			System.out.println("\n\nNumber of queens: " + n + "\n");
-			// initialize/reset counters to 0
-			total_checked = 0;
-			total_solutions = 0;
-			// to store our solution in temporarily
+
+			// start counters at 0
+			checkedCount = 0;
+			solutionCount = 0;
+			// to build a solution
 			int[] temp = new int[n];
 
 			// function call to get estimate
-			long estimated = estimate(n);
+			long estimate = estimate(n);
 
+			// function call to solve
 			try {
-				// function call to solve
 				solve(temp, 0);
 			} catch (Exception e) {
 				System.out.println("Error while solving problem");
 				e.printStackTrace();
 			}
-
 			// print results
-			System.out.println("\nNumber of solutions: " + total_solutions);
-			System.out.println("Estimated nodes checked: " + estimated);
-			System.out.println("Actual nodes checked: " + total_checked);
-
+			System.out.println("\nNumber of solutions: " + solutionCount);
+			System.out.println("Estimated nodes checked: " + estimate);
+			System.out.println("Actual nodes checked: " + checkedCount);
 			// prompt for user input in console
 			System.out.print("\n\nEnter number of queens (between 1 and 13): ");
-
 			// grab user input
 			n = input.nextInt();
-
 		}
 		input.close();
 	}
@@ -113,13 +113,13 @@ public class Main {
 				printSolution(x);
 
 				// increment solution counter
-				total_solutions++;
+				solutionCount++;
 			} else
 				// for this row, check each column position to see if queen can be placed
 				for (int i = 1; i <= x.length; i++) {
 					x[k] = i;
 					// increment counter for the total number of nodes checked while searching
-					total_checked++;
+					checkedCount++;
 					// check if current position is promising
 					if (promising(x, k)) {
 						// if promising, then go down in tree (next row)
@@ -133,11 +133,13 @@ public class Main {
 
 	/**
 	 * Determines if node does not lead to a solution based on from conflicts with
-	 * earlier values. If a node is decided to be a dead end, the method returns
-	 * false, otherwise it returns true.
+	 * earlier values, which means there is a queen already placed in that row, col,
+	 * or diagonal. If a node is decided to be a dead end, the method returns false,
+	 * otherwise it returns true.
 	 *
 	 * @param x the current partial solution
 	 * @param i the position to be tested for conflict with earlier values
+	 * @return false if node conflicts with earlier values, true otherwise
 	 */
 	public static boolean promising(int[] x, int i) {
 		// check if any queen threatens the queen in the ith row
@@ -153,9 +155,9 @@ public class Main {
 	/**
 	 * Uses the Monte Carlo technique to estimate the number of nodes that will be
 	 * in the pruned state space tree, or the number of nodes that will be checked
-	 * before finding all possible solutions. This algorithm follows a random path 
-	 * in a state space tree and produces an estimate of the number of nodes in the 
-	 * tree. 
+	 * before finding all possible solutions. This algorithm follows a random path
+	 * in a state space tree and produces an estimate of the number of nodes in the
+	 * tree.
 	 *
 	 * @param n number of queens
 	 * @return estimated number of nodes in the pruned state space tree
@@ -163,19 +165,27 @@ public class Main {
 	public static long estimate(int n) {
 		// indices for col[]
 		int i = 0, j = 1;
-		// for calculating estimate
-		long m = 1, mprod = 1, numNodes = 1;
+
+		/* estimate variables need to start at 1 or else mprod would always be 0 */
+
+		// number of promising children
+		long m = 1;
+		// product of mprod and m (part of function to find estimate)
+		long mprod = 1;
+		// number of nodes in the estimation of state space tree size
+		long nodeCount = 1;
+
 		// create current col
 		col = new int[n + 1];
 		// for storing promising children (length of factor value for n)
-		promising_children = new int[n];
+		est = new int[n];
 
-		/* ---------------------- Monte Carlo technique ----------------------- */
+		/* ---------------------- Monte Carlo function ----------------------- */
 		while (m != 0 && i != n) {
-			// product of promising children
-			mprod = mprod * m;
-			// n is number of children of t
-			numNodes = numNodes + mprod * n;
+			// get product of promising children and m
+			mprod = m * m;
+			// function to get estimate (n is number of children of current node)
+			nodeCount = nodeCount + mprod * n;
 			i++;
 			m = 0;
 			// determine promising children
@@ -187,22 +197,25 @@ public class Main {
 					// increment number of promising children
 					m++;
 					// add to set of promising children
-					promising_children[(int) m] = j;
+					est[(int) m] = j;
 				}
 			}
-			// if there are promising children
-			if (m != 0) {
-				// randomly selection from promising children
-				int rnd = new Random().nextInt(promising_children.length);
-				j = promising_children[rnd];
+			// check if there are promising children
+			if (nodeCount != 0) {
+				// randomly select from promising children
+				int rnd = new Random().nextInt(est.length);
+				j = est[rnd];
 				col[i] = j;
 			}
 		}
-		return numNodes;
+		// return size of estimated tree
+		return nodeCount;
 	}
 
 	/**
 	 * Prints a solution to the n-queens problem
+	 * 
+	 * @param x a single solution to print
 	 */
 	public static void printSolution(int[] x) {
 		for (int i = 0; i < x.length; i++) {
